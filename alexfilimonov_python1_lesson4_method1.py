10956f974d55dd5e7cce47e59add75abe8f5777e
alexandrefilimonov <alexflido@yahoo.com>
2018-10-17 22:58:50 -0700

    Python1 Lesson4 HW

---
+++ alexfilimonov_python1_lesson4_method1.py
@@ -0,0 +1,35 @@
+﻿#C:\Users\CA32132\AppData\Local\Programs\Python\Python37\python.exe C:\alex\_________________________________________________NewCode\_Python1Lesson4\alexfilimonov_python1_lesson4_method1.py
+#import random
+Small=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+Capital=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+# Задание-1:
+# Вывести символы в нижнем регистре, которые находятся вокруг
+# 1 или более символов в верхнем регистре.
+# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
+       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
+       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
+       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'\
+       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'\
+       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'\
+       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'\
+       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'\
+       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'\
+       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'\
+       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'\
+       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'\
+       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
+       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
+       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+n = len(line)
+i = 1
+c = "m"
+for i in range(n):
+    if (line[i] in Small) :
+        c=c+line[i]
+    else :
+        print(c)
+        c = ""
+print(c)
+
+
---
+++ alexfilimonov_python1_lesson4_method3.py
@@ -0,0 +1,43 @@
+﻿#C:\Users\CA32132\AppData\Local\Programs\Python\Python37\python.exe C:\alex\_________________________________________________NewCode\_Python1Lesson4\alexfilimonov_python1_lesson4_method3.py 
+# Задание-3:
+# Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
+# произвольными целыми цифрами, в результате в файле должно быть
+# 2500-значное произвольное число.
+# Найдите и выведите самую длинную последовательность одинаковых цифр
+# в вышезаполненном файле.
+
+from random import *
+i=0 
+n=2500 
+numb0=""
+while (i<n) :
+    numb0+=str(randint(0,9))
+    i+=1
+#print(numb)
+
+fw = open("testfile.txt", "w") 
+fw.write(numb0) 
+fw.close()
+fr = open("testfile.txt", "r")
+numb=fr.read() 
+
+k=1
+max=0
+ch=numb[0]
+outstring=""
+for i in range(1,n):
+    if (ch==numb[i]) : 
+        k+=1
+    else : 
+        if (k>max) :
+            max=k
+            outstring=(str(ch)*k)
+        k=1
+        ch=numb[i]
+
+if (k>max) :
+    max=k
+fr.close()
+print('Max string is ', outstring);
+
+
